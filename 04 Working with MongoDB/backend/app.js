d37ea3ccb4f73255bf1c048eef03f08125e2bff6
Qasim Khan <78582000+qasimk2120@users.noreply.github.com>
2024-06-26 22:39:27 +0300

    Section 4 Added

---
+++ 04 Working with MongoDB/backend/app.js
@@ -0,0 +1,66 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+const Post = require("./models/post");
+
+const app = express();
+mongoose
+  .connect(
+    "mongodb+srv://qasimk2:pIxanpR1cxrO4jkQ@cluster0.sohvcbf.mongodb.net/MyMessagePost?retryWrites=true&w=majority&appName=Cluster0"
+  )
+  .then(() => {
+    console.log("Connected to database!");
+  })
+  .catch(() => {
+    console.log("connection to database failed");
+  });
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With, Content-Type"
+  );
+  next();
+});
+
+app.post("/api/posts", (req, res, next) => {
+  const post = new Post({
+    title: req.body.title,
+    content: req.body.content,
+  });
+  post.save().then((createdPost) => {
+    res.status(201).json({ message: "Post Added successfully", postId: createdPost._id });
+  });
+});
+
+app.get("/api/posts", (req, res, next) => {
+  Post.find().then((documents) => {
+    res.status(200).json({
+      message: "Post fetched Successfully!",
+      posts: documents,
+    });
+  });
+});
+
+app.delete("/api/posts/:id", (req, res, next) => {
+  Post.deleteOne({ _id: req.params.id })
+    .then((result) => {
+      console.log(result);
+      res.status(200).json({
+        message: "Post deleted Successfully!",
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+module.exports = app;
---
+++ 04 Working with MongoDB/src/app/posts/post-list/post-list.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { Post } from '../post.model';
+import { PostsService } from '../posts.service';
+
+@Component({
+  selector: 'app-post-list',
+  templateUrl: './post-list.component.html',
+  styleUrls: ['./post-list.component.css']
+})
+export class PostListComponent implements OnInit, OnDestroy {
+  posts: Post[] = [];
+  private postsSub :Subscription;
+  constructor(public postsService: PostsService) {
+    
+  }
+  ngOnInit(): void {
+    this.postsService.getPosts();
+    this.postsSub = this.postsService.getPostUpdateListener()
+    .subscribe((posts: Post[]) => {
+      this.posts = posts;
+    });
+  }
+  onDelete(postId: string){
+    this.postsService.onDelete(postId);
+  }
+
+
+  ngOnDestroy(): void {
+    this.postsSub.unsubscribe();
+  }
+
+}
