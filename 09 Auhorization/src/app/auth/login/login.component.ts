bd7dde60954671db65d269ae4c8af2544f7db97f
Qasim Khan <78582000+qasimk2120@users.noreply.github.com>
2024-07-17 19:06:59 +0300

    Section 09 Added, plus functions moved to Controllers folder within seperate files for user and post routes to follow MVC pattern and a better simplified approach

---
+++ 09 Auhorization/src/app/auth/login/login.component.ts
@@ -0,0 +1,20 @@
+import { Component, inject } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { AuthService } from '../auth.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent {
+  isLoading = false;
+  public authService= inject(AuthService)
+  onLogin(form: NgForm) {
+    if(form.invalid) {
+      return;
+    }
+    this.isLoading = true;
+    this.authService.login(form.value.email, form.value.password);
+  }
+}
---
+++ 09 Auhorization/src/app/auth/signup/signup.component.ts
@@ -0,0 +1,20 @@
+import { Component, inject } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { AuthService } from '../auth.service';
+
+@Component({
+  selector: 'app-signup',
+  templateUrl: './signup.component.html',
+  styleUrl: './signup.component.css'
+})
+export class SignupComponent {
+  isLoading = false;
+  public authService = inject(AuthService)
+  onSignUp(form: NgForm) {
+    if(form.invalid) {
+      return;
+    }
+    this.isLoading = true;
+    this.authService.createUser(form.value.email, form.value.password);
+}
+}
